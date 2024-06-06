ca4130580242270552bcc5b1e1dfb7937ade954c
Anas Hatem <74430931+anashatem46@users.noreply.github.com>
2024-06-06 03:50:53 +0300

    -AppBar Added With Search

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -2,19 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rickandmorty/business_logic/cubit/character_cubit.dart';
 import 'package:rickandmorty/constants/colors.dart';
-
 import '../../data/models/characters.dart';
 import '../widgets/character_item.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+
   @override
   State<HomeScreen> createState() => _HomeScreenState();
+
+
+
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   List<Character>? allCharacters;
+  List <Character>? searchedCharacters;
+  bool isSearching = false;
+  final  searchController = TextEditingController();
+
 
   @override
   void initState() {
@@ -51,7 +58,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return GridView.builder(
       shrinkWrap: true,
       physics: const ClampingScrollPhysics(),
-      itemCount: allCharacters!.length,
+      itemCount: searchController.text.isEmpty ? allCharacters!.length : searchedCharacters!.length,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 2/3,
@@ -60,18 +67,99 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       itemBuilder: (BuildContext context, int index) {
         return CharacterItem(
-          character: allCharacters![index],
+          character: searchController.text.isEmpty? allCharacters![index]: searchedCharacters![index],
         );
       },
     );
   }
 
+
+  Widget buildSearchField() {
+    return TextField(
+      controller: searchController,
+      cursorColor: MyColors.myMainGreen,
+      decoration: const InputDecoration(
+        hintText: 'Search for a character',
+        hintStyle: TextStyle(color: MyColors.myMainGreen),
+        border: InputBorder.none,
+
+      ),
+      onChanged: (searchedCharacter) {
+        addSearchedCharactersToSearchedList(searchedCharacter);
+      },
+    );
+  }
+
+
+  void addSearchedCharactersToSearchedList(String searchedCharacter) {
+    searchedCharacters = allCharacters!
+        .where((character) =>
+        character.name.toLowerCase().contains(searchedCharacter.toLowerCase()))
+        .toList();
+    setState(() {
+
+    });
+  }
+
+
+  List <Widget> appBarAction( ) {
+    if (isSearching) {
+      return [
+        IconButton(
+          onPressed: () {
+            searchController.clear();
+            Navigator.pop(context);
+            setState(() {
+              isSearching = false;
+            });
+          },
+          icon: const Icon(Icons.clear),
+        ),
+      ];
+    } else {
+      return [
+         IconButton(
+          onPressed: startSearching,
+          icon: const Icon(Icons.search),
+        ),
+      ];
+    }
+
+  }
+
+  void startSearching() {
+    ModalRoute.of(context)!.addLocalHistoryEntry(LocalHistoryEntry(onRemove: _stopSearching));
+    setState(() {
+      isSearching = true;
+    });
+  }
+
+void _stopSearching() {
+    setState(() {
+      searchController.clear();
+      isSearching = false;
+    });
+}
+
+
+
+
+Widget _buildAppBarTitle() {
+    return const Text('Rick and Morty Characters',
+        style: TextStyle(color: MyColors.myMainGreen));
+  }
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Rick and Morty Characters',
-              style: TextStyle(color: MyColors.myMainGreen),),
+          title: isSearching ? buildSearchField() : _buildAppBarTitle(),
+          actions: appBarAction(),
         ),
         body: buildBlocWidget(),
         bottomNavigationBar:
---
+++ lib/presentation/widgets/search_field.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:rickandmorty/constants/colors.dart';
+
+
+
+
+
